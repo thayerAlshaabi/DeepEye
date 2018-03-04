37fec822da85c47e7f664f920402a3f3ba0e1207
Thayer <thayerAlshaabi@users.noreply.github.com>
2018-03-04 16:20:58 -0500

    added cProfiling script

---
+++ src/profiler.py
@@ -0,0 +1,26 @@
+
+import os, sys
+sys.path.append("..")
+
+PROFILER_STATS_PATH = os.path.join(os.getcwd(), 'profiler')
+
+_generate_cProfile_cmd = "python3 -m cProfile -o " + \
+    os.path.join(PROFILER_STATS_PATH, 'profile.pstats') + \
+    " -s module main.py"
+
+os.system(_generate_cProfile_cmd)
+
+_generate_pGraph_cmd = "gprof2dot -f pstats " + \
+    os.path.join(PROFILER_STATS_PATH, 'profile.pstats') + \
+    " > " + \
+    os.path.join(PROFILER_STATS_PATH, 'map.svg')
+    
+
+os.system(_generate_pGraph_cmd)
+
+_show_ptable_cmd = "cprofilev -f " + \
+    os.path.join(PROFILER_STATS_PATH, 'profile.pstats') + \
+    " | start chrome http://127.0.0.1:4000"
+
+os.system(_show_ptable_cmd)
+
